91fae386e4b64b2d96e77089941f9a5db6dab9b6
garwoodl <126724039+garwoodl@users.noreply.github.com>
2024-04-26 18:47:08 -0700

    beginning classes

--- game.py
+++ game.py
@@ -1,3 +1,21 @@
 '''
-File to implement the game Toads and Frogs backend
+File to implement the game Toads and Frogs
 '''
+
+
+class ToadAndFrogs:
+    '''
+    Magnages game aspects such as
+    - board state
+    - verifying legal moves
+    - updating with move input
+    - checking for wins
+    '''
+
+    def __init__(self, starting_state: list[str]):
+        '''
+        Constructor for the ToadsAndFrogs class
+        starting_state should be a list of the form ['F', '-', 'T']
+        '''
+        self.board_state = starting_state
+        self.board_size = len(self.board_state)
